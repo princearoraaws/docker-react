6cf5196ee0c26088f086fcaef5ec51c8817591d9
prince arora <prince.arora.aws@gmail.com>
2021-01-30 04:35:43 +0530

    Updated gitlab yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <b>Hello Prince  Arora...  Yo !!!!!!</b>
+          <b>Good Morning Prince</b>
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload
